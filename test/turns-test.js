fa24eaa17bbe381b9cfcf6012d6f719c211a034f
Rachel Prather <rachelprather1@gmail.com>
2023-05-09 17:05:02 -0700

    feat: Add test for correct guess

--- test/turns-test.js
+++ test/turns-test.js
@@ -1,3 +1,24 @@
 const chai = require('chai');
 const expect = chai.expect;
+const { evaluateGuess } = require('../src/turns');
 
+describe('evaluateGuess function', () => {
+  it('should return a message if the answer is correct', () => {
+    const card = {
+      id: 1,
+      question: 'What is my name?', 
+      answers: ['Rachel', 'Katherine', 'Bell'],
+      correctAnswer: 'Rachel'
+    };
+    const guess = 'Rachel';
+
+    const result = evaluateGuess(guess, card);
+
+    expect(result).to.equal('correct!');
+  });
+
+  // it('should return a different message if the answer is incorrect', () => {
+
+  // });
+
+});
