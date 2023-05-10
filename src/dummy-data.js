6f58400db54b7fe09c088a62a2bb1715c8c07faa
Rachel Prather <rachelprather1@gmail.com>
2023-05-10 14:27:48 -0700

    feat: Create file with dummy data for tests

---
+++ src/dummy-data.js
@@ -0,0 +1,18 @@
+sampleCards = [{
+  id: 1,
+  question: 'What is my name?',
+  answers: ['Rachel', 'Katherine', 'Bell'],
+  correctAnswer: 'Rachel'
+}, {
+  id: 2,
+  question: 'What is my sign?',
+  answers: ['Scorpio', 'Cancer', 'Aries'],
+  correctAnswer: 'Cancer'
+}, {
+  id: 3,
+  question: 'What is my occupation?',
+  answers: ['astronaut', 'dancer', 'student'],
+  correctAnswer: 'student'
+}];
+
+module.exports = { sampleCards };
