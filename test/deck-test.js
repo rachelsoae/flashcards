b4ced0ed41fcb7fff2ca325e793dd6c2a0746dfa
Rachel Prather <rachelprather1@gmail.com>
2023-05-10 14:47:51 -0700

    feat: Create test for new deck

---
+++ test/deck-test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const expect = chai.expect;
+const { createDeck, countCards } = require('../src/deck');
+const { sampleCards } = require('../src/dummy-data');
+
+describe('createDeck function', () => {
+  it('should return an array of cards', () => {
+    const cards = [sampleCards[0], sampleCards[1], sampleCards[2]];
+
+    let deck = createDeck(cards);
+
+    expect(deck).to.deep.equal([{
+      id: 1,
+      question: 'What is my name?',
+      answers: ['Rachel', 'Katherine', 'Bell'],
+      correctAnswer: 'Rachel'
+    }, {
+      id: 2,
+      question: 'What is my sign?',
+      answers: ['Scorpio', 'Cancer', 'Aries'],
+      correctAnswer: 'Cancer'
+    }, {
+      id: 3,
+      question: 'What is my occupation?',
+      answers: ['astronaut', 'dancer', 'student'],
+      correctAnswer: 'student'
+    }])
+  });
+});
