df3de02e99d135121a0eafc86074e6e9cabb4348
Rachel Prather <rachelprather1@gmail.com>
2023-05-11 12:29:48 -0700

    refactor: Rename Card-test to card-test; updated functions to arrow fns

--- test/Card-test.js
+++ test/Card-test.js
@@ -3,12 +3,12 @@ const expect = chai.expect;
 
 const { createCard } = require('../src/card');
 
-describe('card', function() {
-  it('should be a function', function() {
+describe('create card function', () => {
+  it('should be a function', () => {
     expect(createCard).to.be.a('function');
   });
 
-  it('should create a card and its properties', function() {
+  it('should create a card and its properties', () => {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     
     expect(card.id).to.equal(1);
