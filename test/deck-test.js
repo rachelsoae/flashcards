4aac9ba11289a9153e957bc85768966a7d96ba0e
Rachel Prather <rachelprather1@gmail.com>
2023-05-11 12:41:11 -0700

    refactor: Test that createDeck is dynamic

--- test/deck-test.js
+++ test/deck-test.js
@@ -32,6 +32,24 @@ describe('createDeck function', () => {
     }])
   });
 
+  it('should return a different array of cards', () => {
+    cards.shift();
+
+    const deck = createDeck(cards);
+    
+    expect(deck).to.deep.equal([{
+      id: 2,
+      question: 'What is my sign?',
+      answers: ['Scorpio', 'Cancer', 'Aries'],
+      correctAnswer: 'Cancer'
+    }, {
+      id: 3,
+      question: 'What is my occupation?',
+      answers: ['astronaut', 'dancer', 'student'],
+      correctAnswer: 'student'
+    }]);
+  })
+
   it('should count the number of cards in a deck', () => {
     const deck = createDeck(cards);
     const numCards = countCards(deck);
